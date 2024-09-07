f30e7dcc6cd501b48c988954e716350733bd395e
Somunsm <1373699746@qq.com>
2024-09-07 13:53:00 +0800

    add 增加窗口最大尺寸限制，防止超出最大屏幕的尺寸

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, globalShortcut} = require('electron')
+const {app, BrowserWindow, screen} = require('electron')
 const path = require("node:path");
 const {ipcHandler} = require("./ipcHandler")
 const BetterSqlite3 = require("better-sqlite3");
@@ -10,25 +10,57 @@ const checkTableExists = require("./DbUtil");
 let window;
 // 数据库
 let db;
+// 最大屏幕的尺寸
+let maxScreenSize = {x: 0, y: 0, pixels: 0}
+
 
 // 窗口设定
 const createWindow = () => {
-    window = new BrowserWindow({
-        minWidth: 800, minHeight: 520, icon: path.join(__dirname, "../public/favicon.ico"), webPreferences: {
-            // 使用Node的path模块引入预加载层
-            preload: path.join(__dirname, "preload.js"),
+    // 获取尺寸最大的屏幕(防止超出边界)
+    let screens = screen.getAllDisplays()
+    for (let i = 0; i < screens.length; i++) {
+        const {workArea} = screens[i];
+        const nowScreenPixels = workArea.width * workArea.height;
+        if (nowScreenPixels > maxScreenSize.pixels) {
+            maxScreenSize = {
+                x: workArea.width,
+                y: workArea.height,
+                pixels: nowScreenPixels
+            };
+        }
+    }
 
-            // 更严格的渲染与Node隔离，防止渲染层恶意访问Node
-            // 当该值为true时，Electron才会将preload.js的对象加载到渲染层中
-            // 例如：React的页面 | 原生Html的renderer.js中
-            contextIsolation: true, // 渲染进程能否访问Node环境（为了安全，建议关闭 |但这不影响渲染层的使用）
-            nodeIntegration: false, // 禁用同源策略,允许跨域
-            webSecurity: false
-        }, // 隐藏标题栏
-        frame: false, // 禁止窗口拖动成自定义尺寸
+    window = new BrowserWindow({
+        minWidth: 800,
+        minHeight: 520,
+        maxWidth: maxScreenSize.x,
+        maxHeight: maxScreenSize.y,
+        icon: path.join(__dirname, "../public/favicon.ico"),
+        webPreferences:
+            {
+                // 使用Node的path模块引入预加载层
+                preload: path.join(__dirname, "preload.js"),
+
+                // 更严格的渲染与Node隔离，防止渲染层恶意访问Node
+                // 当该值为true时，Electron才会将preload.js的对象加载到渲染层中
+                // 例如：React的页面 | 原生Html的renderer.js中
+                contextIsolation:
+                    true,
+                // 渲染进程能否访问Node环境（为了安全，建议关闭 |但这不影响渲染层的使用）
+                nodeIntegration:
+                    false,
+                // 禁用同源策略,允许跨域
+                webSecurity:
+                    false
+            },
+        // 隐藏标题栏
+        frame: false,
+        // 禁止窗口拖动成自定义尺寸
         // resizable: false,
         // 隐藏窗口顶部菜单栏
-        autoHideMenuBar: true, // 窗口底色
+        autoHideMenuBar:
+            true,
+        // 窗口底色
         // backgroundColor: "red"
     })
 
