178b759ba4c70b4c631a51703acf98f485b1db38
Somunsm <1373699746@qq.com>
2024-09-08 21:24:06 +0800

    add 页面增加显示错误的路由地址 add 增加返回主页按钮 优化 增加部分样式

--- src/page/error/404/index.js
+++ src/page/error/404/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {useLocation} from "react-router";
+import {Error404Style} from "./style";
+import {useNavigate} from "react-router-dom";
+import {PAGE_MAP} from "../../../router/PageMap";
 
 const Error404 = () => {
+
+    // 路由信息
+    let location = useLocation();
+    // 路由跳转
+    let navigate = useNavigate();
+
+    // 回到主页
+    const toHome = () => {
+        navigate(PAGE_MAP.home.path);
+    }
+
     return (
-        <div>
-            404！
-        </div>
+        <Error404Style>
+            <h1>404</h1>
+            <p className={"routerPath"}>"{location.pathname}"</p>
+            <p>这地址你从哪搞来的</p>
+            <button onClick={toHome}>快跑</button>
+        </Error404Style>
     );
 };
 
---
+++ src/page/error/404/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Error404Style = styled.div`
+
+  text-align: center;
+  font-size: xxx-large;
+
+  .routerPath {
+    user-select: text;
+  }
+
+`
