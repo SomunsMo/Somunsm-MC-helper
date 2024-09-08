17d91e4d032c20cfaf8787303871dd71688a640e
Somunsm <1373699746@qq.com>
2024-09-09 00:33:32 +0800

    change 将"当前预览的资源信息"改为"当前预览的资源索引"

--- src/store/assetStore.js
+++ src/store/assetStore.js
@@ -4,8 +4,8 @@ import {makeAutoObservable, toJS} from "mobx";
 export const DEFAULT_TEXT = {
     // 资源列表
     assets: null,
-    // 当前预览的资源信息
-    assetInfo: null,
+    // 当前预览的资源索引
+    viewerAssetIndex: null,
 
 };
 
@@ -15,8 +15,8 @@ export const DEFAULT_TEXT = {
 class AssetStore {
     // 资源列表 - 从文件中读取的数据
     assets = DEFAULT_TEXT.assets;
-    // 当前预览的资源信息
-    assetInfo = DEFAULT_TEXT.assetInfo;
+    // 当前预览的资源索引
+    viewerAssetIndex = DEFAULT_TEXT.viewerAssetIndex;
 
     constructor() {
         //自动把数据改为响应式
@@ -41,6 +41,18 @@ class AssetStore {
         this.assets = latestAssets;
     }
 
+
+    getViewerAssetIndex = () => {
+        return this.viewerAssetIndex;
+    };
+
+    setViewerAssetIndex = (index) => {
+        if (this.viewerAssetIndex === index) {
+            return;
+        }
+
+        this.viewerAssetIndex = index;
+    };
 }
 
 export default AssetStore;
