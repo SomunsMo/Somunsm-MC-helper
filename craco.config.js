85a078acef1e660dc4276ad08d7c1f037d9f99b2
Somunsm <1373699746@qq.com>
2024-09-09 18:28:47 +0800

    fixed 修复打包后网页请求资源路径错误的Bug

--- craco.config.js
+++ craco.config.js
@@ -7,6 +7,16 @@ module.exports = {
         alias: {
             //xx: resolve("目标路径（从根目录开始）")
             "@": resolve("src"),
+        },
+        // 修改craco打包后存放的位置
+        configure: (webpackConfig, {env, paths}) => {
+            // 修改build的生成文件名称
+            paths.appBuild = 'out';
+            webpackConfig.output = {
+                ...webpackConfig.output,
+                path: path.resolve(__dirname, 'out'),
+            }
+            return webpackConfig;
         }
     },
     babel: {
