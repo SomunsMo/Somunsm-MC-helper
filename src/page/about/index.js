e65aeb2df52e26a4985de0c7ad4b37ecd0c25e40
Somunsm <1373699746@qq.com>
2024-09-10 14:53:18 +0800

    optimize 地址集中在一个常量中 optimize 当前版本 改为 软件版本 add NodeJs、Chrome、Electron版本信息

--- src/page/about/index.js
+++ src/page/about/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {AboutStyle} from "./style";
 import Description from "../../components/description";
+import useStore from "../../store/rootStore";
+
+// 地址列表
+const URL = {
+    // 创建人B站主页
+    creatorBilibili: "https://space.bilibili.com/66932246",
+    // 开源地址
+    openSource: "https://github.com/SomunsMo/Somunsm-MC-helper",
+
+};
 
 const About = () => {
+    const {ConfigStore} = useStore();
+
+    // 软件及依赖的版本信息
+    const [appVer, setAppVer] = useState({})
+
+    useEffect(() => {
+        setAppVer(ConfigStore.getAppVer());
+    }, []);
 
     // 默认浏览器打开链接
     const openUrl = (url) => {
@@ -21,12 +39,19 @@ const About = () => {
     return (
         <AboutStyle>
             <h4>关于</h4>
-            <Description name={"创建人"} content={<span onClick={toBilibili}>索姆斯小鼠</span>} inline={true}/>
-            <Description name={"开源地址"} inline={true}/>
-            <Description name={"开源协议"} inline={true}/>
-            <Description name={"当前版本"} content={"1.0.0"} inline={true}/>
-
-            <a href="javascript:void(0)" onClick={toBilibili}>创建人B站</a>
+            <Description name={"创建人"}
+                         content={getLink(URL.creatorBilibili, "索姆斯小鼠", "前往创建人的B站主页")}
+                         inline={true}/>
+            <Description name={"开源地址"}
+                         content={getLink(URL.openSource, "Somunsm-MC-helper", "前往开源仓库")}
+                         inline={true}/>
+            <Description name={"开源协议"} content={"MIT (The Massachusetts Institute of TechnologyLicense)"}
+                         inline={true}/>
+            <Description name={"软件版本"} content={appVer.app} inline={true}/>
+            <hr/>
+            <Description name={"NodeJs"} content={appVer.node} inline={true}/>
+            <Description name={"Chrome"} content={appVer.chrome} inline={true}/>
+            <Description name={"Electron"} content={appVer.electron} inline={true}/>
         </AboutStyle>
     );
 };
